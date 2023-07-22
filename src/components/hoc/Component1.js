bd457e7fb6481a32b367cf1d43f7a2c40dafc50c
kumarmudur <kumarmudur10@gmail.com>
2023-07-22 07:33:56 -0400

    [ReactJS] hoc patterns examples

---
+++ src/components/hoc/Component1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Component1 = () => {
+    return (
+        <div className="comp1">
+            Hey I am Component1 
+        </div>
+    );
+};
+
+export default Component1;
---
+++ src/components/hoc/Component2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Component2 = () => {
+    return (
+        <div className="comp2">
+            Hey I am Component2 
+        </div>
+    );
+};
+
+export default Component2;
